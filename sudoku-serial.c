8ccd533a1af0c256786f3543cfc885c31e05ef90
Manuel Serra <manuelserranunes@gmail.com>
2018-03-25 21:25:37 +0100

    máscara na versão iterativa

--- sudoku-serial.c
+++ sudoku-serial.c
@@ -14,105 +14,77 @@ typedef struct{
 
 int r_size, m_size;
 
-int read_matrix(t_array *grid, int argc, char *argv[]){
-    FILE *fp;
-    size_t len = 0;
-    ssize_t read;
-    int i, j, k, l, aux2;
-    int rooted_matrix_size, n_matrix_size;
-    char *line = NULL, aux[3];
+int read_matrix(t_array *grid, int argc, char *argv[]);
+int exists_in( int index, int* mask, int num);
+int is_safe_num( int* rows_mask, int* cols_mask, int* boxes_mask, int row, int col, int num);
+int solve(int **grid, int m_zeros, int* rows_mask, int* cols_mask, int* boxes_mask);
+int new_mask( int size);
+int int_to_mask(int num);
+void init_masks(int** grid, int* rows_mask, int* cols_mask, int* boxes_mask);
+void update_masks(int num, int row, int col, int* rows_mask, int* cols_mask, int* boxes_mask);
+int nr_zeros(int **grid);
 
-    //verifies if the program calling has at least one argument
-    if (argc<1) {
-        fprintf(stderr, "usage: %s filename\n", argv[0]);
-        exit(1);
-    }
+
+int main(int argc, char *argv[]) {
+    clock_t start, end, result;
+    t_array grid1;
+    int i, j;
     
-    //if it has, opens the file
-    if (argc > 1) {
-        fprintf(stderr, "opening file %s\n", argv[1]);
+    if(argc == 2){
+        printf("\n");
+        r_size = read_matrix(&grid1, argc, argv);
+        m_size = r_size * r_size;
+
+        int* rows_mask = (int*) malloc(m_size * sizeof(int));
+        int* cols_mask = (int*) malloc(m_size * sizeof(int));
+        int* boxes_mask = (int*) malloc(m_size * sizeof(int));
+
+        printf("\ninitial sudoku:");
+        print_grid(grid1.arr, r_size, m_size);
+        printf("number of zeros:%d\n\n", nr_zeros(grid1.arr));
+        printf("result sudoku:");
         
-        //verifies if the file was correctly opened
-        if ((fp = fopen(argv[1], "r+")) == NULL) {
-            fprintf(stderr, "unable to open file %s\n", argv[1]);
-            exit(1);
-        }else
-            fprintf(stderr, "%s opened\n",argv[1]);
-    }
+        //start measurement
+        start = clock();
+        
+        init_masks(grid1.arr, rows_mask, cols_mask, boxes_mask);
+        result = solve(grid1.arr, nr_zeros(grid1.arr), rows_mask, cols_mask, boxes_mask);
+        
+        // end measurement
+        end = clock();
+        
+        print_grid(grid1.arr, r_size, m_size);
+        
+        // mesmo que nao esteja completo verifica se esta correto (apenas para testes)
+        verify_sudoku(grid1.arr, m_size, r_size) == 1 ? printf("rigth!\n") : printf("wrong!\n");
+        
+        // verifica se tem solucao ou nao
+        result == 1 ? printf("solved!\n") : printf("no solution!\n");
+        
+        printf("it took %lf sec.\n\n",(double) (end-start)/CLOCKS_PER_SEC);
+    }else
+        printf("invalid input arguments.\n");
     
-    if(read = getline(&line, &len, fp) != -1)
-        rooted_matrix_size = atoi(line);
-    
-    n_matrix_size = rooted_matrix_size *rooted_matrix_size;
-
-    grid->arr = (int **)malloc(n_matrix_size * sizeof(int*));
-    
-    for(i = 0; i < n_matrix_size; i++)
-        grid->arr[i] = (int*) malloc(n_matrix_size * sizeof(int));
-
-    for(i = 0; getline(&line, &len, fp) != -1; i++){
-        k = 0;
-        l = 0;
-        for (j = 0; line[j] != '\n'; j++) {
-            if(line[j] == ' ' || line[j] == '\n'){
-                aux[l] = '\0';
-                grid->arr[i][k] = atoi(aux);
-                l = 0;
-                k++;
-            }else{
-                aux[l] = line[j];
-                l++;
-            }
-        }
-    }
-
-    fclose(fp);
-    return rooted_matrix_size;
-}
-
-int is_exist_row(int **grid, int row, int num){
-    int col;
-
-    for (col = 0; col < m_size; col++)
-        if (grid[row][col] == num)
-            return 1;
     return 0;
 }
 
-int is_exist_col(int **grid, int col, int num) {
-    int row;
+int exists_in( int index, int* mask, int num){
 
-    for (row = 0; row < m_size; row++)
-        if (grid[row][col] == num)
-            return 1;
-    return 0;
-}
+    int res;
+    int masked_num = int_to_mask(num);
 
-int is_exist_box(int **grid, int startRow, int startCol, int num) {
-    int row, col;
-
-    for (row = 0; row < r_size; row++)
-        for (col = 0; col < r_size; col++)
-            if (grid[row + startRow][col + startCol] == num)
-                return 1;
-    return 0;
-}
-
-int is_safe_num(int **grid, int row, int col, int num) {
-    return !is_exist_row(grid, row, num) && !is_exist_col(grid, col, num) && !is_exist_box(grid, row - (row % r_size), col - (col %r_size), num);
-}
-
-int nr_zeros(int **grid) {
-    int i, j, cont = 0;
+    res = mask[index] | masked_num; 
+    if( res != mask[index]) return 0;
     
-    for (i = 0; i < m_size; i++)
-        for (j = 0; j < m_size; j++)
-            if (!grid[i][j])
-                cont++;
-    return cont;
+    return 1;
 }
 
-int solve(int **grid, int m_zeros) {
+int is_safe_num( int* rows_mask, int* cols_mask, int* boxes_mask, int row, int col, int num) {
+
+    return !exists_in( row, rows_mask, num) && !exists_in( col, cols_mask, num) && !exists_in( r_size*(row/r_size)+col/r_size, boxes_mask, num);
+} 
+
+int solve(int **grid, int m_zeros, int* rows_mask, int* cols_mask, int* boxes_mask) {
     int back_values[m_zeros][3];
     int row, col, val;
     int zeros = 1, flag_back = 0, cont_back = 0;
@@ -152,7 +124,7 @@ int solve(int **grid, int m_zeros) {
                     }
                     
                     for(; val <= m_size; val++){
-                        if(is_safe_num(grid, row, col, val)){
+                        if(is_safe_num( rows_mask, cols_mask, boxes_mask, row, col, val)){
                             back_values[cont_back][ROW]=row;
                             back_values[cont_back][COL]=col;
                             back_values[cont_back][VAL]=val;
@@ -175,52 +147,109 @@ int solve(int **grid, int m_zeros) {
     return 1;
 }
 
-/* final print function */
+int new_mask(int size){
+    int mask = 0 << (size-1);
+    return mask;
+}
 
-/*void print_grid(int **grid) {
-    int row, col;
+int int_to_mask(int num){
+    int mask = 1 << (num-1);
+    return mask;
+}
 
-    for (row = 0; row < m_size; row++) {
-        for (col = 0; col < m_size; col++)
-            printf("%d ", grid[row][col]);
-        printf("\n");
+void init_masks(int** grid, int* rows_mask, int* cols_mask, int* boxes_mask){
+
+    int mask;
+
+    for(int i = 0; i < m_size; i++){
+        rows_mask[i] = 0; 
+        cols_mask[i] = 0; 
+        boxes_mask[i] = 0; 
     }
-}*/
 
-int main(int argc, char *argv[]) {
-    clock_t start, end, result;
-    t_array grid1;
-    int i, j;
-    
-    if(argc == 2){
-        printf("\n");
-        r_size = read_matrix(&grid1, argc, argv);
-        m_size = r_size * r_size;
+    for( int row = 0; row < m_size; row++){
+        for(int col = 0; col < m_size; col++){
 
-        printf("\ninitial sudoku:");
-        print_grid(grid1.arr, r_size, m_size);
-        printf("number of zeros:%d\n\n", nr_zeros(grid1.arr));
-        printf("result sudoku:");
-        
-        //start measurement
-        start = clock();
-        
-        result = solve(grid1.arr, nr_zeros(grid1.arr));
-        
-        // end measurement
-        end = clock();
-        
-        print_grid(grid1.arr, r_size, m_size);
-        
-        // mesmo que nao esteja completo verifica se esta correto (apenas para testes)
-        verify_sudoku(grid1.arr, m_size, r_size) == 1 ? printf("rigth!\n") : printf("wrong!\n");
-        
-        // verifica se tem solucao ou nao
-        result == 1 ? printf("solved!\n") : printf("no solution!\n");
-        
-        printf("it took %lf sec.\n\n",(double) (end-start)/CLOCKS_PER_SEC);
-    }else
-        printf("invalid input arguments.\n");
+            //if the cell has a number add that number to the current row, col and box mask
+            if(grid[row][col] !=0 ){
+                mask = int_to_mask( grid[row][col] ); //convert number found to mask ex: if dim=4x4, 3 = 0010 
+                rows_mask[row] = rows_mask[row] | mask; //to add the new number to the current row's mask use bitwise OR 
+                cols_mask[col] = cols_mask[col] | mask;    
+                boxes_mask[r_size*(row/r_size)+col/r_size] = boxes_mask[r_size*(row/r_size)+col/r_size] | mask;    
+            }
+        }        
+    }
+} 
+
+void update_masks(int num, int row, int col, int* rows_mask, int* cols_mask, int* boxes_mask){
+    int new_mask = int_to_mask(num);
+    rows_mask[row] = rows_mask[row] | new_mask;
+    cols_mask[col] = cols_mask[col] | new_mask;
+    boxes_mask[r_size*(row/r_size)+col/r_size] = boxes_mask[r_size*(row/r_size)+col/r_size] | new_mask;
+}
+
+int nr_zeros(int **grid) {
+    int i, j, cont = 0;
     
-    return 0;
+    for (i = 0; i < m_size; i++)
+        for (j = 0; j < m_size; j++)
+            if (!grid[i][j])
+                cont++;
+    return cont;
+}
+
+int read_matrix(t_array *grid, int argc, char *argv[]){
+    FILE *fp;
+    size_t len = 0;
+    ssize_t read;
+    int i, j, k, l, aux2;
+    int rooted_matrix_size, n_matrix_size;
+    char *line = NULL, aux[3];
+
+    //verifies if the program calling has at least one argument
+    if (argc<1) {
+        fprintf(stderr, "usage: %s filename\n", argv[0]);
+        exit(1);
+    }
+    
+    //if it has, opens the file
+    if (argc > 1) {
+        fprintf(stderr, "opening file %s\n", argv[1]);
+        
+        //verifies if the file was correctly opened
+        if ((fp = fopen(argv[1], "r+")) == NULL) {
+            fprintf(stderr, "unable to open file %s\n", argv[1]);
+            exit(1);
+        }else
+            fprintf(stderr, "%s opened\n",argv[1]);
+    }
+    
+    if( (read = getline(&line, &len, fp)) != -1)
+        rooted_matrix_size = atoi(line);
+    
+    n_matrix_size = rooted_matrix_size *rooted_matrix_size;
+
+    grid->arr = (int **)malloc(n_matrix_size * sizeof(int*));
+    
+    for(i = 0; i < n_matrix_size; i++)
+        grid->arr[i] = (int*) malloc(n_matrix_size * sizeof(int));
+
+    for(i = 0; getline(&line, &len, fp) != -1; i++){
+        k = 0;
+        l = 0;
+        for (j = 0; line[j] != '\n'; j++) {
+            if(line[j] == ' ' || line[j] == '\n'){
+                aux[l] = '\0';
+                grid->arr[i][k] = atoi(aux);
+                l = 0;
+                k++;
+            }else{
+                aux[l] = line[j];
+                l++;
+            }
+        }
+    }
+
+    fclose(fp);
+    return rooted_matrix_size;
 }
