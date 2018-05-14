ab7ce4f1306e275c951dc5f8c33c58997b0193f1
josecvieira <josecarlosvieira@tecnico.ulisboa.pt>
2018-05-14 18:49:42 +0100

    mpi

--- mpi/sudoku-mpi-v2.c
+++ mpi/sudoku-mpi-v2.c
@@ -228,6 +228,7 @@ int solve_from(int* cp_sudoku, uint64_t* rows_mask, uint64_t* cols_mask, uint64_
         }
         
         MPI_Test(&request, &flag, &status);
+		int bosta = status.MPI_SOURCE;
         if(flag){
             if(status.MPI_TAG == TAG_EXIT){
                 printf("[%d] process = %d asked to terminate\n", id, status.MPI_SOURCE);
@@ -242,9 +243,10 @@ int solve_from(int* cp_sudoku, uint64_t* rows_mask, uint64_t* cols_mask, uint64_
                     send_msg[POS] = hyp.cell;
                     send_msg[VAL] = hyp.num;
                     memcpy(&send_msg[2], cp_sudoku, v_size*sizeof(int));
-                    
-                    MPI_Send(send_msg, v_size+2, MPI_INT, 3, TAG_HYP, MPI_COMM_WORLD);
-                    printf("[%d] sent work to process %d\n", id, status.MPI_SOURCE);
+					printf("[%d] sent work to process %d\n", id, bosta);                    
+		
+                    MPI_Send(send_msg, v_size+2, MPI_INT, bosta, TAG_HYP, MPI_COMM_WORLD);
+                    printf("[%d] sent work to process %d\n", id, bosta);
                     
                     free(send_msg);
                 }else
