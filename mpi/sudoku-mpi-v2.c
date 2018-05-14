6930e91a237e974b50cd73189428eeea22a4f0f2
jose vieira <josecarlosvieira@tecnico.ulisboa.pt>
2018-05-14 08:49:08 +0100

    mpi

--- mpi/sudoku-mpi-v2.c
+++ mpi/sudoku-mpi-v2.c
@@ -234,8 +234,8 @@ int solve_from(int* cp_sudoku, uint64_t* rows_mask, uint64_t* cols_mask, uint64_
                     printf("[%d] process = %d asked for a job\n", id, status.MPI_SOURCE);
                     MPI_Isend(response, 1, MPI_INT, status.MPI_SOURCE, TAG_HYP, MPI_COMM_WORLD, &request);
                     
-                    response[POS] = 123;
-                    response[VAL] = 123;
+                    response[POS] = hyp.cell;
+                    response[VAL] = hyp.num;
                                         
                     /*Item hyp_send = pop_head(work);
                     response[POS] = hyp_send.cell;
