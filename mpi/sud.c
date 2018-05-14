021e892cbdce65e952f27111ed60ed8ff3827367
jose vieira <josecarlosvieira@tecnico.ulisboa.pt>
2018-05-14 22:42:39 +0100

    mpi

--- mpi/sud.c
+++ mpi/sud.c
@@ -244,7 +244,7 @@ int solve_from(int* cp_sudoku, uint64_t* rows_mask, uint64_t* cols_mask, uint64_
                     hyp_send.cell = 10;
                     hyp_send.num = 20;
                     
-                    memcpy(send_msg, &hyp_send, sizeof(Item));
+                    memcpy(send_msg, &hyp, sizeof(Item));
                     memcpy((send_msg+2), cp_sudoku, v_size*sizeof(int));
                     
                     MPI_Send(send_msg, (v_size+2), MPI_INT, status.MPI_SOURCE, TAG_HYP, MPI_COMM_WORLD);
