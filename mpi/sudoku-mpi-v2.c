9f3be7031f6dd248d577696b0c9c31bab1e262fc
jose vieira <josecarlosvieira@tecnico.ulisboa.pt>
2018-05-14 10:56:22 +0100

    mpi

--- mpi/sudoku-mpi-v2.c
+++ mpi/sudoku-mpi-v2.c
@@ -150,11 +150,11 @@ int solve(int* sudoku){
                 
                 for(i = 0; i < p; i++)
                     if(i != id)
-                        MPI_Isend(&i, 1, MPI_INT, i, TAG_ASK_JOB, MPI_COMM_WORLD, &request_send[i]);
+                        MPI_Send(&i, 1, MPI_INT, i, TAG_ASK_JOB, MPI_COMM_WORLD);
                     
-                for(i = 0; i < p; i++)
+                /*for(i = 0; i < p; i++)
                     if(i != id)
-                        MPI_Wait(&request_send[i], &status_send[i]);
+                        MPI_Wait(&request_send[i], &status_send[i]);*/
                 
                 MPI_Irecv(&recv, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &request_recv);
                 flag = 0;
