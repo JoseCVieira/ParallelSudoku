b82adf6a690600e0750561cf8c7596f356bd53d2
jose vieira <josecarlosvieira@tecnico.ulisboa.pt>
2018-05-15 17:17:33 +0100

    mpi

--- mpi/sudoku-mpi.c
+++ mpi/sudoku-mpi.c
@@ -195,9 +195,9 @@ int solve(int* sudoku){
                                 
                                 //printf("[%d] received work size=%d, cell = %d, val = %d\n", id, number_amount, hyp_recv.cell, hyp_recv.num);
                                 
-                                delete_from(sudoku, cp_sudoku, r_mask_array, c_mask_array, b_mask_array, hyp_recv.cell);
+                                //delete_from(sudoku, cp_sudoku, r_mask_array, c_mask_array, b_mask_array, hyp_recv.cell);
                                 
-                                insert_head(work, hyp_recv);
+                                //insert_head(work, hyp_recv);
                                 //flag_enter = 1;
                                 free(number_buf);
                                 break;
