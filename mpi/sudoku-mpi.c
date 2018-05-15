ccf756734bd62f00c5031186244e893ac3b30dc8
jose vieira <josecarlosvieira@tecnico.ulisboa.pt>
2018-05-15 17:05:04 +0100

    mpi

--- mpi/sudoku-mpi.c
+++ mpi/sudoku-mpi.c
@@ -281,10 +281,12 @@ int solve_from(int* cp_sudoku, uint64_t* rows_mask, uint64_t* cols_mask, uint64_
                             
                             printf("[%d] SOLUTION\n", id);
                             
-                            MPI_Test(&request, &flag, &status);
+                            /*MPI_Test(&request, &flag, &status);
                             MPI_Irecv(&recv, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &request_t);
                             if(!flag)
-                                MPI_Cancel(&request);
+                                MPI_Cancel(&request);*/
+                            while(1)
+                                sleep(1);
                             return 1;
                          }
                         
