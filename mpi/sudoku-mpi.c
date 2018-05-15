6e99ec27ca5a065d268f36d425910c3cc8b17f85
jose vieira <josecarlosvieira@tecnico.ulisboa.pt>
2018-05-15 18:52:29 +0100

    mpi

--- mpi/sudoku-mpi.c
+++ mpi/sudoku-mpi.c
@@ -342,8 +342,8 @@ void delete_from(int* sudoku, int *cp_sudoku, uint64_t* rows_mask, uint64_t* col
         if(cp_sudoku[i] > 0)
             update_masks(cp_sudoku[i], ROW(i), COL(i), rows_mask, cols_mask, boxes_mask);
         
-    print_sudoku(cp_sudoku);
-    printf("\n");
+    //print_sudoku(cp_sudoku);
+    //printf("\n");
 }
 
 int exists_in(int index, uint64_t* mask, int num) {
