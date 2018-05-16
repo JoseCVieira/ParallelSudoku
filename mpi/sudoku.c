038854e5b210e208443f2ddab1705d981e02448a
jose vieira <josecarlosvieira@tecnico.ulisboa.pt>
2018-05-16 17:44:23 +0100

    mpii

--- mpi/sudoku.c
+++ mpi/sudoku.c
@@ -226,7 +226,7 @@ if(start_pos == -1)
 int solve_from(int* sudoku, int* cp_sudoku, uint64_t* rows_mask, uint64_t* cols_mask, uint64_t* boxes_mask, List* work, int last_pos, int last) {
     int cell, val, recv, flag;
     
-    int number_amount, no_job, flag, data;
+    int number_amount, no_job, data;
     
     MPI_Request request;
     MPI_Status status;
