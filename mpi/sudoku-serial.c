a423b49e8fd39db4e45741ba0725f1c7a206bcb5
jose vieira <josecarlosvieira@tecnico.ulisboa.pt>
2018-05-17 23:03:22 +0100

    mpi

--- mpi/sudoku-serial.c
+++ mpi/sudoku-serial.c
@@ -264,7 +264,8 @@ int solve_from(int* sudoku, int* cp_sudoku, uint64_t* rows_mask, uint64_t* cols_
         if(i == id){
             for(i = 0; i < p-1; i++){
                 printf("[%d] vai sari\n", id);
-                MPI_Recv(number_buf, number_amount, MPI_INT, status.MPI_SOURCE, TAG_ASK_JOB, MPI_COMM_WORLD, &status);
+                int abc;
+                MPI_Recv(abc, 1, MPI_INT, status.MPI_SOURCE, TAG_ASK_JOB, MPI_COMM_WORLD, &status);
                 MPI_Send(&no_hyp, 2, MPI_INT, status.MPI_SOURCE, TAG_HYP, MPI_COMM_WORLD);
             }
             return 0;
