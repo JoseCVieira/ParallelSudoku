9fe152b479a0b71d6bb3bb3ea936652d3449f177
jose vieira <josecarlosvieira@tecnico.ulisboa.pt>
2018-05-18 00:00:20 +0100

    mpi

--- mpi/sudoku-serial.c
+++ mpi/sudoku-serial.c
@@ -263,8 +263,11 @@ int solve_from(int* sudoku, int* cp_sudoku, uint64_t* rows_mask, uint64_t* cols_
         }
         
         if(i == id){
-            printf("[%d]\n", id);
-            send_ring(&id, TAG_NO_SOL, -1);
+            for(i = id+1; i != id; i++){
+                if(i == p) i = 0;
+                int msg_send;
+                MPI_Send(msg_send, 1, MPI_INT, i, TAG_NO_SOL, MPI_COMM_WORLD);
+            }
             return 0;
         }
     }
