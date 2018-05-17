9ea0a4f5732a89edabc51b1fa06daa5eafd0706f
jose vieira <josecarlosvieira@tecnico.ulisboa.pt>
2018-05-17 17:38:57 +0100

    mpi

--- mpi/sudoku-serial.c
+++ mpi/sudoku-serial.c
@@ -183,6 +183,7 @@ int solve_from(int* sudoku, int* cp_sudoku, uint64_t* rows_mask, uint64_t* cols_
                                         while(!flag)
                                             MPI_Iprobe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &flag, &status);
                                         
+                                        flag = 0;
                                         MPI_Get_count(&status, MPI_INT, &number_amount);
                                         int* number_buf = (int*)malloc(number_amount * sizeof(int));
                                         MPI_Recv(number_buf, number_amount, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
@@ -254,8 +255,9 @@ int solve_from(int* sudoku, int* cp_sudoku, uint64_t* rows_mask, uint64_t* cols_
             free(number_buf);
         }
         
-        while(work->head == NULL){
-            sleep(1);
+        if(work->head == NULL)
+            return 0;
+            /*sleep(1);
             printf("[%d] No solution\n", id);
             flag = 0;
             while(!flag)
@@ -266,7 +268,7 @@ int solve_from(int* sudoku, int* cp_sudoku, uint64_t* rows_mask, uint64_t* cols_
             MPI_Recv(number_buf, number_amount, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
             MPI_Send(&no_hyp, 2, MPI_INT, status.MPI_SOURCE, TAG_HYP, MPI_COMM_WORLD);
             free(number_buf);
-        }
+        }*/
     }
 }
 
