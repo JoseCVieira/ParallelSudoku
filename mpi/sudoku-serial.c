0a8c139ee80db7e84df36e3b0df2fe2369dc1501
jose vieira <josecarlosvieira@tecnico.ulisboa.pt>
2018-05-17 15:55:38 +0100

    mpi

--- mpi/sudoku-serial.c
+++ mpi/sudoku-serial.c
@@ -228,8 +228,6 @@ int solve_from(int* sudoku, int* cp_sudoku, uint64_t* rows_mask, uint64_t* cols_
         if(i == id)
             continue;
 
-        MPI_Send(&i, 1, MPI_INT, i, TAG_ASK_JOB, MPI_COMM_WORLD);
-        
         MPI_Test(&request, &flag, &status);
         if(flag){
             flag = 0;
@@ -237,6 +235,7 @@ int solve_from(int* sudoku, int* cp_sudoku, uint64_t* rows_mask, uint64_t* cols_
         }else
             MPI_Cancel(&request);
         
+        MPI_Send(&i, 1, MPI_INT, i, TAG_ASK_JOB, MPI_COMM_WORLD);
         MPI_Probe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
         MPI_Get_count(&status, MPI_INT, &number_amount);
         int* number_buf = (int*)malloc(number_amount * sizeof(int));
