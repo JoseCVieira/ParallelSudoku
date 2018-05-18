a6549ca320ec3cdb2448b2b9bb09d6d50b14be3a
jose vieira <josecarlosvieira@tecnico.ulisboa.pt>
2018-05-18 16:41:30 +0100

    mpi

--- mpi/sudoku-mpi.c
+++ mpi/sudoku-mpi.c
@@ -77,6 +77,9 @@ int solve(int* sudoku){
     int i, last_pos, flag_start = 0, solved = 0;
     Item hyp;
     
+    //init work lists, one for each process.
+    //A work list contains the pairs (cell_id, number) from wich serial DFS search must still be performed
+    //(in other words it cointains the root values of the unexplored parts of the search tree)
     uint64_t *r_mask_array = (uint64_t*) malloc(m_size * sizeof(uint64_t));
     uint64_t *c_mask_array = (uint64_t*) malloc(m_size * sizeof(uint64_t));
     uint64_t *b_mask_array = (uint64_t*) malloc(m_size * sizeof(uint64_t));
@@ -98,14 +101,20 @@ int solve(int* sudoku){
 
     init_masks(sudoku, r_mask_array, c_mask_array, b_mask_array);
     
-    for(i = 1 + BLOCK_LOW(id, p, m_size); i < 2 + BLOCK_HIGH(id, p, m_size); i++){
+    //calculate the low and high values for the first cell for each precess
+    //and insert it in the work list
+    for(i = 1 + BLOCK_HIGH(id, p, m_size); i >= 1 + BLOCK_LOW(id, p, m_size); i--){
         hyp.num = i;
         insert_head(work, hyp);
     }
+    
+    print_list(work);
 
+    // try to solve sudoku
     solved = solve_from(sudoku, cp_sudoku, r_mask_array, c_mask_array, b_mask_array, work, last_pos);
 
     if(solved){
+        //if the solution is found copy the solution to be retrieved
         #pragma omp parallel for
         for(i = 0; i < v_size; i++)
             if(cp_sudoku[i] != UNCHANGEABLE)
@@ -251,21 +260,27 @@ int solve_from(int* sudoku, int* cp_sudoku, uint64_t* rows_mask, uint64_t* cols_
     }
 }
 
+//when a process receives a new hypothesis and the corresponding cp_sudoku, then we has to clear all the work
+//that the sender process have done after the hypothesis's position
 void delete_from(int* sudoku, int *cp_sudoku, uint64_t* rows_mask, uint64_t* cols_mask, uint64_t* boxes_mask, int cell){
     int i;
     
+    //initialize the masks from the sudoku read from the file
     init_masks(sudoku, rows_mask, cols_mask, boxes_mask);
     
+    //clear all work done by the sender process
     #pragma omp parallel for
     for(i = v_size; i >= cell; i--)
         if(cp_sudoku[i] > 0)
             cp_sudoku[i] = UNASSIGNED;
 
+    //initialize the masks from the cp_sudoku received
     for(i = 0; i < cell; i++)
         if(cp_sudoku[i] > 0)
             update_masks(cp_sudoku[i], ROW(i), COL(i), rows_mask, cols_mask, boxes_mask);
 }
 
+//create an invalid hypothesis
 Item invalid_hyp(void){
     Item item;
     item.cell = -1;
@@ -284,27 +299,32 @@ void send_ring(void *msg, int tag, int dest){
         MPI_Send(msg_send, 2, MPI_INT, id+1, tag, MPI_COMM_WORLD);
 }
 
+//test if a number (num) exists in a row, column or box. To check if it exists in a row, the mask of that row is passed as input
 int exists_in(int index, uint64_t* mask, int num) {
     int res, masked_num = int_to_mask(num);
 
-    res = mask[index] | masked_num;
-    if(res != mask[index])
+    res = mask[index] | masked_num;  //example row_mask = 1010 ; number to check = 0100 (3) then row_masl | num = 1110
+    if(res != mask[index])           // since the result is different from the initial mask, it is safe to put 3 in this row/col/box
         return 0;
     return 1;
 }
 
+//check if a given number is safe in a given cell (row,col) for that check safety in corresponding row, column and box
 int is_safe_num(uint64_t* rows_mask, uint64_t* cols_mask, uint64_t* boxes_mask, int row, int col, int num) {
     return !exists_in(row, rows_mask, num) && !exists_in(col, cols_mask, num) && !exists_in(BOX(row, col), boxes_mask, num);
 }
 
+//initialize a mask
 int new_mask(int size) {
     return (0 << (size-1));
 }
 
+//convert an integer to mask ex: 3 = 0100
 int int_to_mask(int num) {
     return (1 << (num-1));
 }
 
+//initialize the masks from the sudoku read from the file
 void init_masks(int* sudoku, uint64_t* rows_mask, uint64_t* cols_mask, uint64_t* boxes_mask) {
     int i;
 
@@ -320,6 +340,7 @@ void init_masks(int* sudoku, uint64_t* rows_mask, uint64_t* cols_mask, uint64_t*
             update_masks(sudoku[i], ROW(i), COL(i), rows_mask, cols_mask, boxes_mask);
 }
 
+//remove number from the masks
 void rm_num_masks(int num, int row, int col, uint64_t* rows_mask, uint64_t* cols_mask, uint64_t* boxes_mask) {
     int num_mask = int_to_mask(num);
     rows_mask[row] ^= num_mask;
@@ -327,13 +348,15 @@ void rm_num_masks(int num, int row, int col, uint64_t* rows_mask, uint64_t* cols
     boxes_mask[BOX(row, col)] ^= num_mask;
 }
 
+//add new number to the masks
 void update_masks(int num, int row, int col, uint64_t* rows_mask, uint64_t* cols_mask, uint64_t* boxes_mask) {
-    int new_mask = int_to_mask(num);
-    rows_mask[row] |= new_mask;
-    cols_mask[col] |= new_mask;
+    int new_mask = int_to_mask(num); //convert number found to mask ex: if dim=4x4, 3 = 0010
+    rows_mask[row] |= new_mask;      //to add the new number to the current row's mask use bitwise OR
+    cols_mask[col] |= new_mask;      //ex row_mask = 0101 ; number to add: 0010 --> row_mask OR num = 0111
     boxes_mask[BOX(row, col)] |= new_mask;
 }
 
+//read the input file
 int* read_matrix(char *argv[]) {
     FILE *fp;
     size_t characters, len = 1;
