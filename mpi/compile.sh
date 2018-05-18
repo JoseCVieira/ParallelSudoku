d629bec65d0ae1bdc1616fa7d07577b07e744f86
jose vieira <josecarlosvieira@tecnico.ulisboa.pt>
2018-05-18 17:29:47 +0100

    serial

--- mpi/compile.sh
+++ mpi/compile.sh
@@ -20,4 +20,5 @@ elif [ "$1" -eq 7 ]; then
     mpirun --host cpd-1,cpd-2,cpd-3,cpd-4,cpd-5,cpd-6,cpd-7,cpd-8 sudoku-mpi ../sudokus/$2
 elif [ "$1" -eq 8 ]; then
     mpirun --host cpd-1,cpd-2,cpd-3,cpd-4,cpd-5,cpd-6,cpd-7,cpd-8,cpd-9 sudoku-mpi ../sudokus/$2
+fi
 ##mpirun -np $1 sudoku-mpi ../sudokus/$2
