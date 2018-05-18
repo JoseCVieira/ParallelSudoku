7da39e4bd592cd3b83189bcda85cbae613ddd7a9
jose vieira <josecarlosvieira@tecnico.ulisboa.pt>
2018-05-18 13:33:30 +0100

    mpi

--- mpi/sudoku-mpi.c
+++ mpi/sudoku-mpi.c
@@ -134,8 +134,6 @@ int solve_from(int* sudoku, int* cp_sudoku, uint64_t* rows_mask, uint64_t* cols_
             hyp = pop_head(work);
             len = work->len;
             int start_pos = hyp.cell;
-            
-            printf("len=%d, it=%d\n", len, nr_it);
 
             if(!is_safe_num(rows_mask, cols_mask, boxes_mask, ROW(hyp.cell), COL(hyp.cell), hyp.num))
                 continue;
@@ -200,6 +198,7 @@ int solve_from(int* sudoku, int* cp_sudoku, uint64_t* rows_mask, uint64_t* cols_
                         }
                     break;
                 }
+                
                 hyp = pop_head(work);
                 
                 for(cell--; cell >= hyp.cell; cell--){
